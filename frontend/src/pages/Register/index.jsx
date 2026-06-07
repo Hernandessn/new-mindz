7b1ef509038f92c77523d6d5cbd0b31956cd96ed
hernandessn <hernandessalesneves7@gmail.com>
2026-06-07 11:35:10 -0300

    feat: add register page with auth integration

---
+++ frontend/src/pages/Register/index.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import { api } from '../../services/api';
+import {
+    LoginForm,
+    Input,
+    Container,
+    Float,
+    FloatLabel,
+    Button
+} from "./styles";
+
+export const Register = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+
+    const navigate = useNavigate();
+
+
+    const handleSubmit = async (e) => {
+        try {
+            e.preventDefault();
+            const { data } = await api.post('/auth/register', { name, email, password });
+            navigate('/login');
+        } catch (error) {
+            console.error(error);
+        }
+
+    }
+
+    return (
+        <Container>
+            <LoginForm onSubmit={handleSubmit}>
+                <Float>
+                    <Input
+                        type="text"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                        placeholder=" "
+                        required
+                    />
+                    <FloatLabel>Nome</FloatLabel>
+                </Float>
+                <Float>
+                    <Input
+                        type="email"
+                        placeholder=" "
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        required
+                    />
+                    <FloatLabel>Email</FloatLabel>
+                </Float>
+
+                <Float>
+                    <Input
+                        type="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        placeholder=" "
+                        required
+                    />
+                    <FloatLabel>Senha</FloatLabel>
+                </Float>
+                <Button type="submit">Criar conta</Button>
+
+            </LoginForm>
+        </Container>
+    );
+};
