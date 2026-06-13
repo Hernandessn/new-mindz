3d7861827d705d362e0c318355120f83c9c6773a
hernandessn <hernandessalesneves7@gmail.com>
2026-06-13 11:26:40 -0300

    feat: add Session model

---
+++ backend/models/Session.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const sessionSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    contentId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Content',
+        required: true
+    },
+    interactions: [
+        {
+            neuronId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Neuron',
+                required: true
+            },
+            responseTime: {
+                type: Number,
+                required: true
+            },
+            correct: {
+                type: Boolean,
+                required: true
+            }
+        }
+    ],
+    startedAt: {
+        type: Date,
+        default: Date.now
+    },
+    endedAt: {
+        type: Date
+    }
+})
+
+export default mongoose.model("Session", sessionSchema);
